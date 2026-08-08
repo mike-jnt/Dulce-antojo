bcb8bba8c8d10a0dc7328e034d7993f5fbe14f2e
mike-jnt <josenatanaelramosrojo203@gmail.com>
2026-08-07 21:57:45 -0500

    Delete js directory

--- js/sw-register.js
+++
@@ -1,7 +0,0 @@
-import { APP_VERSION } from "./config/app-config.js";
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register(`./sw.js?v=${encodeURIComponent(APP_VERSION)}`).catch(() => {});
-  });
-}
